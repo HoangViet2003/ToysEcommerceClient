75f197a4149cd3c3aa1318f454db5fc47b01a419
HoangViet2003 <nguyenhoangviet10d1@gmail.com>
2023-06-21 17:25:32 +0700

    fix route6

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 
-const ENDPOINT = 'http://localhost:8000/api'
-// const ENDPOINT = "https://drippyecommerce.onrender.com/api";
+// const ENDPOINT = 'http://localhost:8000/api'
+const ENDPOINT = "https://drippyecommerce.onrender.com/api";
 const accessToken = localStorage.getItem("accessToken");
 
 
